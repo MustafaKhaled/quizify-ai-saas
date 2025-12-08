1b33ed2902d285f3e23b00cc6540dbb30e4c8552
Mustafa Khaled <m.khaled@rewe-group.at>
2025-12-08 20:27:04 +0100

    Add DATABASE_URL validation and update .gitignore for __pycache__ exclusion

--- backend/db/database.py
+++ backend/db/database.py
@@ -5,6 +5,10 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
+if DATABASE_URL is None:
+    # This exception should catch the failure before SQLAlchemy does
+    raise Exception("DATABASE_URL environment variable is missing!")
+
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- backend/db/models.py
+++ backend/db/models.py
@@ -8,7 +8,7 @@ from sqlalchemy import (
 )
 from sqlalchemy.dialects.postgresql import UUID, JSONB
 from sqlalchemy.orm import relationship
-from db.database import Base
+from .database import Base
 
 
 def generate_uuid():
