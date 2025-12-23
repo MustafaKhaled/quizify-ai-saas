1918bad4e4a8cf6a1eafbcd79aed48a3224bf973
Mustafa Khaled <m.khaled@rewe-group.at>
2025-12-24 00:08:17 +0100

    Added delete method

--- backend/db/routers/admin_router.py
+++ backend/db/routers/admin_router.py
@@ -16,18 +16,24 @@ CurrentAdmin = Annotated[models.User, Depends(get_current_admin)]
 @router.get("/allusers", response_model=list[UserAdminResponse])
 async def get_all_users(
     db: db_dep,
-    current_user: CurrentAdmin
+    _: CurrentAdmin
 ):
     users = db.query(models.User).all()
     return users
 
 
-@router.delete("/user/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_user(user_id: str, db: db_dep):
-    user = db.query(models.User).filter(models.User.id == user_id).first()
+@router.delete("/user/email/{email}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user_by_email(
+    email: str,
+    db: db_dep,
+    _: CurrentAdmin
+):
+    user = db.query(models.User).filter(models.User.email == email).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    
+
     db.delete(user)
     db.commit()
-    return None
+
+
+
