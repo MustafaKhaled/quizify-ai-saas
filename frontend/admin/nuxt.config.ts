30679f3aa37303dccdbbf0bac598ab3730a82a9e
Mustafa Khaled <mustafakhaledmustafa@outlook.com>
2026-01-20 02:48:47 +0100

    add auth and cookie

--- frontend/admin/nuxt.config.ts
+++ frontend/admin/nuxt.config.ts
@@ -28,20 +28,31 @@ export default defineNuxtConfig({
       }
     }
   },
-  runtimeConfig: {
-    public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE_URL
-    },
+  auth: {
     session: {
       name: 'nuxt-session',
       password: process.env.NUXT_SESSION_PASSWORD,
       cookie: {
-        sameSite: 'lax', 
-        secure: true, // Railway is HTTPS, so this should be true
+        sameSite: 'lax',
+        secure: true,
         httpOnly: true,
-        path: '/', // Ensure it's available for all routes
+        path: '/'
       }
-    }
+    },
+  cookie: {
+  sameSite: 'lax',
+  secure: true, 
+  httpOnly: true,
+  path: '/',
+  // This allows the cookie to work even if Railway's proxy 
+  // doesn't pass the 'secure' flag perfectly to the container
+  partitioned: true 
+}
+  },
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE_URL
+    },
   },
   future: { compatibilityVersion: 4 }
 })
