432b6aa8a6c24ed8b784f4beae76bfae653b1182
Mustafa Khaled <mustafakhaledmustafa@outlook.com>
2026-03-28 16:14:41 +0100

    cleanup marketing webpage

--- frontend/marketing/app/components/AppFooter.vue
+++ frontend/marketing/app/components/AppFooter.vue
@@ -1,34 +1,10 @@
 <script setup lang="ts">
 const columns = [{
-  label: 'Resources',
-  children: [{
-    label: 'Help center'
-  }, {
-    label: 'Docs'
-  }, {
-    label: 'Roadmap'
-  }, {
-    label: 'Changelog'
-  }]
-}, {
-  label: 'Features',
-  children: [{
-    label: 'Affiliates'
-  }, {
-    label: 'Portal'
-  }, {
-    label: 'Jobs'
-  }, {
-    label: 'Sponsors'
-  }]
-}, {
   label: 'Company',
   children: [{
     label: 'About'
   }, {
     label: 'Pricing'
-  }, {
-    label: 'Careers'
   }, {
     label: 'Blog'
   }]
@@ -59,38 +35,13 @@ function onSubmit() {
     <template #top>
       <UContainer>
         <UFooterColumns :columns="columns">
-          <template #right>
-            <form @submit.prevent="onSubmit">
-              <UFormField
-                name="email"
-                label="Subscribe to our newsletter"
-                size="lg"
-              >
-                <UInput
-                  v-model="email"
-                  type="email"
-                  class="w-full"
-                  placeholder="Enter your email"
-                >
-                  <template #trailing>
-                    <UButton
-                      type="submit"
-                      size="xs"
-                      color="neutral"
-                      label="Subscribe"
-                    />
-                  </template>
-                </UInput>
-              </UFormField>
-            </form>
-          </template>
         </UFooterColumns>
       </UContainer>
     </template>
 
     <template #left>
       <p class="text-muted text-sm">
-        Built with Nuxt UI • © {{ new Date().getFullYear() }}
+        Quizify • © {{ new Date().getFullYear() }}
       </p>
     </template>
 
